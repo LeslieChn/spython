fab17f08ba0ea05aea8837d2a817f655ff274706
Leslie Chang <zz2724@columbia.edu>
2024-04-11 14:34:52 -0400

    fix indent logic

--- src/util.ml
+++ src/util.ml
@@ -48,20 +48,39 @@ let get_indent_width (lst: token list list)  =
 
 let indent_to_scope (counted_lst: (int * token list) list) =
     let st = Stack.create () in 
-    let rec indent_or_dedent counted_lst = 
+    let rec indent_or_dedent counted_lst dedenting = 
         let indent_width = fst counted_lst in
         let lst_data = snd counted_lst in
-        let st_top = Stack.top st in
-        if indent_width > st_top then begin
-            Stack.push indent_width st;
-            LBRACE :: lst_data
-        end
-        else if indent_width < (Stack.top st) then begin
-            Stack.pop st;
-            indent_or_dedent (indent_width, RBRACE :: lst_data)
-        end 
-        else
-            lst_data
+        if Stack.is_empty st then
+            (fprintf stderr "indentation error\n"; exit 1)
+        else ( 
+            let st_top = Stack.top st in
+            if dedenting then
+            (
+                if indent_width > st_top then
+                    (fprintf stderr "indentation error\n"; exit 1)
+                else if indent_width < st_top then (
+                    Stack.pop st;
+                    indent_or_dedent (indent_width, RBRACE :: lst_data) true
+                )
+                else (
+                    lst_data
+                )
+            )
+            else (
+                if indent_width > st_top then (
+                    Stack.push indent_width st;
+                    LBRACE :: lst_data
+                )
+                else if indent_width < st_top then (
+                    Stack.pop st;
+                    indent_or_dedent (indent_width, RBRACE :: lst_data) true
+                ) 
+                else
+                    (* indent_width *)
+                    lst_data 
+            )
+        )
     in
     let rec flush_indent acc = 
         if Stack.length st > 1 then (Stack.pop st; flush_indent (RBRACE :: acc))
@@ -71,36 +90,8 @@ let indent_to_scope (counted_lst: (int * token list) list) =
     let rec go lst acc = 
         match lst with
         [] -> List.rev ((flush_indent []) :: acc)
-        | hd :: tl -> go tl ((indent_or_dedent hd) :: acc)
+        | hd :: tl -> go tl ((indent_or_dedent hd false) :: acc)
     in Stack.push 0 st; go counted_lst []
-(*
-let convert_to_c (lst : token list list) = 
-    let max_indent = ref 0 in
-    let cur_indent = ref 0 in
-    let indent_start = ref false in
-    let tab_width = 4 in
-    let rec append lst acc =
-        match lst with
-        | [] when !cur_indent > !max_indent -> acc 
-        | [] when !cur_indent < !max_indent -> acc 
-        | [] -> acc 
-        (* during indent *)
-        | hd :: tl when !indent_start = true && hd = TAB ->
-                cur_indent := !cur_indent + tab_width; append tl ([] :: acc)
-        | hd :: tl when !indent_start = true && hd = SPACE ->
-                cur_indent := !cur_indent + 1; append tl ([] :: acc)
-        | hd :: tl when !indent_start = true -> (* hit 1st token after indent *)
-                if !current_indent < append tl ([] :: acc)
-        (* not indent *)
-        | hd :: tl when !indent_start = false && hd = TAB ->
-                indent_start := true; append tl (hd :: acc)
-        | hd :: tl -> append tl (hd :: acc) in
-    let rec flatten lst =
-        match lst with
-        | [] -> [] 
-        | hd :: tl -> append hd (flatten tl) in
-    flatten lst
-*)
 
 
 let print_token_list lst =
